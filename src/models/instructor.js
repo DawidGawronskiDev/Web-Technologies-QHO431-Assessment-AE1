38bb67d8cb9e0239b492f8d2afaf1f7fb5f200e9
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-13 18:23:23 +0100

    update instructor model

--- src/models/instructor.js
+++ src/models/instructor.js
@@ -13,19 +13,6 @@ class Instructor {
       });
     });
   }
-
-  static getById(id) {
-    return new Promise((resolve, reject) => {
-      db.get("SELECT * FROM instructors WHERE id = ?", [id], (err, row) => {
-        if (err) {
-          console.error("Error fetching instructor: ", err.message);
-          reject(err);
-        } else {
-          resolve(row);
-        }
-      });
-    });
-  }
 }
 
 module.exports = Instructor;
