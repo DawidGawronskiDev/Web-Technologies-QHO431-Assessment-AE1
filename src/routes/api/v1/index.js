a565ea7948839eeb2326c8235c873544a46b3d81
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-13 18:24:06 +0100

    update instructors routes

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const courseRoutes = require("./coursesRoutes");
+const instructorRoutes = require("./instructorsRoutes");
 
 const router = express.Router();
 
 router.use("/courses", courseRoutes);
+router.use("/instructors", instructorRoutes);
 
 module.exports = router;
---
+++ src/routes/api/v1/instructorsRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const instructorController = require("../../../controllers/instructors");
+
+const router = express.Router();
+
+router.get("/", instructorController.getInstructors);
+
+module.exports = router;
