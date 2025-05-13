80730d8be23cce6db834f2460d13203fb2189f02
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-13 22:21:40 +0100

    create faqs model

---
+++ src/models/faqs.js
@@ -0,0 +1,16 @@
+const db = require("../lib/db");
+
+module.exports = class Faqs {
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      db.all("SELECT * FROM faqs", [], (err, rows) => {
+        if (err) {
+          console.error("Error fetching FAQs: ", err.message);
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+};
