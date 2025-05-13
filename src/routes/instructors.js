85fc49aec058537f7b69c7d5edc2630de8b8e38d
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-13 18:32:11 +0100

    delete instructors routes

--- src/routes/instructors.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const db = require("../lib/db");
-const { getInstructors } = require("../controllers/instructors");
-
-const router = express.Router();
-
-router.get("/", getInstructors);
-
-module.exports = router;
