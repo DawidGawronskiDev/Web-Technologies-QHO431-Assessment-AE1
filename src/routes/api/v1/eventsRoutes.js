f25c44ee2d6bd4e135099c1b9f5bd4229ce2a01c
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-13 18:30:47 +0100

    create events routes

---
+++ src/routes/api/v1/eventsRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const eventsController = require("../../../controllers/events");
+
+const router = express.Router();
+
+router.get("/", eventsController.getEvents);
+
+module.exports = router;
--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const courseRoutes = require("./coursesRoutes");
 const instructorRoutes = require("./instructorsRoutes");
+const eventsRoutes = require("./eventsRoutes");
 
 const router = express.Router();
 
 router.use("/courses", courseRoutes);
 router.use("/instructors", instructorRoutes);
+router.use("/events", eventsRoutes);
 
 module.exports = router;
