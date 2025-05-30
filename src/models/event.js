85d9df6a16706e1c0aacc01e680d01f1bf8a4749
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-30 15:27:25 +0100

    update events date

--- src/models/event.js
+++ src/models/event.js
@@ -30,7 +30,7 @@ module.exports = class Event {
   static async getEventByInstructorId(instructorId) {
     return new Promise((resolve, reject) => {
       db.all(
-        "SELECT * FROM events WHERE instructor_id = ?",
+        "SELECT e.*, i.name AS instructor_name FROM events e JOIN instructors i ON i.id = e.instructor_id  WHERE instructor_id = ?",
         [instructorId],
         (err, rows) => {
           if (err) {
