b88df15c7d9c03e3a04414ecaadb63729b2bc154
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-05-30 10:11:57 +0100

    update instructors routes

--- src/routes/instructors.js
+++ src/routes/instructors.js
@@ -3,7 +3,8 @@ const instructorController = require("../controllers/instructors");
 
 const router = express.Router();
 
-router.get("/", instructorController.getInstructors);
-router.get("/:id", instructorController.getInstructor);
+router
+  .get("/", instructorController.getInstructors)
+  .get("/:id", instructorController.getInstructor);
 
 module.exports = router;
